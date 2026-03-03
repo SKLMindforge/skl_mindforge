3733d7f4b4fc8965fee57cbdd6c5935b39054537
SKL Mindforge <shreemaniyer9@outlook.com>
2026-03-03 23:21:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skl_mindforge",
-    version="0.1.3",  # BUMP TO 0.1.3
+    version="0.1.4", 
     packages=find_packages(),
     install_requires=[
         "tokenizers>=0.13.0",
@@ -12,6 +12,6 @@ setup(
         "skl_mindforge": ["*.json"],
     },
     author="SKLMindforge",
-    description="Zenith Tokenizer with Ultra-Refined Decoder",
+    description="Zenith Tokenizer with Glue-Logic Decoder",
     url="https://github.com/SKLMindforge/skl_mindforge",
 )
