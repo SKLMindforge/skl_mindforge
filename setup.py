0c4dd969f9f8cc45b8fea7f78aa164999394fbd6
SKL Mindforge <shreemaniyer9@outlook.com>
2026-03-04 00:03:45 +0530

    Bump version to 0.1.6 and update description

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="skl_mindforge",
-    version="0.1.5", 
+    version="0.1.6", 
     packages=find_packages(),
     install_requires=[
         "tokenizers>=0.13.0",
@@ -12,6 +12,6 @@ setup(
         "skl_mindforge": ["*.json"],
     },
     author="SKLMindforge",
-    description="Zenith Tokenizer: Universal Math & Science Edition",
+    description="Zenith Tokenizer: Final Byte-Level Edition",
     url="https://github.com/SKLMindforge/skl_mindforge",
 )
